eb8888e80df39594ba8d9af129f7b8b5a3a266e2
MxInput <mxinput@protonmail.com>
2026-05-30 00:35:18 -0400

    Water robots now work on trees

--- nodes/Robot.gd
+++ nodes/Robot.gd
@@ -18,6 +18,8 @@ var SPEED : float = 50.0
 @onready var plants: TileMapLayer = get_node("/root/Game/Plants")
 @onready var watered: TileMapLayer = get_node("/root/Game/Watered")
 
+@onready var paths_node: Node2D = get_node("/root/Game/Paths")
+
 var astar_grid = AStarGrid2D.new()
 
 func initialize(_grid : AStarGrid2D, _target : Vector2i):
@@ -28,37 +30,58 @@ func initialize(_grid : AStarGrid2D, _target : Vector2i):
 	finished = false
 	
 func _process(delta: float) -> void:
+	if !plants.plant_data.get(target_cell) or tiles.watered_tiles.get(target_cell):
+		velocity = Vector2.ZERO
+		current_cell = tiles.local_to_map(tiles.to_local(global_position))
+		moving = false
+		finished = true
+		move_points.clear()
+		current_point = 0
+		target_cell = Vector2i.ZERO
+			
 	if !finished:
 		if current_cell != target_cell:
 			if !moving:
 				move_points = grid.get_point_path(current_cell, target_cell)
 				move_points = (move_points as Array).map(func (p): return p + grid.cell_size/2.0)
 				start_moving()
+
 		else:
 			finished = true
 
 func start_moving():
 	if move_points.is_empty(): return
 	current_point = 0; moving = true
-	
+
 func _physics_process(delta: float) -> void:
 	if !move_points.is_empty():
 		if current_point == move_points.size() - 2:
+			paths_node.targeted.erase(target_cell)
 			velocity = Vector2.ZERO
 			global_position = move_points[-2]
 			current_cell = tiles.local_to_map(tiles.to_local(global_position))
-			if !tiles.watered_tiles.get(target_cell):
-				if plants.plant_data.get(target_cell):
-					tiles.watered_tiles[target_cell] = {"time": 0}
-					watered.set_cell(target_cell, 0, Vector2i(0,0))
+			if (plants.tree_info.get(plants.plant_data[target_cell]["fruit_name"])):
+				if !tiles.watered_tiles.get(target_cell):
+					if plants.plant_data.get(target_cell):
+						for x in 3:
+							for y in 4:
+								var initial = plants.plant_data[target_cell]["initial"]
+								
+								tiles.watered_tiles[initial + Vector2i(x-1, y-2)] = {"time": 0}
+								watered.set_cell(initial + Vector2i(x-1, y-2), 0, Vector2i.ZERO)
+			else:
+				if !tiles.watered_tiles.get(target_cell):
+					if plants.plant_data.get(target_cell):
+						tiles.watered_tiles[target_cell] = {"time": 0}
+						watered.set_cell(target_cell, 0, Vector2i(0,0))
 			moving = false
 			finished = true
 			move_points.clear()
 			current_point = 0
 			target_cell = Vector2i.ZERO
 		else:
-			var dir = (move_points[current_point + 1] - move_points[current_point]).normalized()
-			
+			var dir = (move_points[current_point + 1] - global_position).normalized()
+
 			var sides = [Vector2.from_angle(PI/4), Vector2.from_angle(3 * PI/4), Vector2.from_angle(5 * PI/4), Vector2.from_angle(7 * PI/4)]
 		
 			match dir:
@@ -78,6 +101,31 @@ func _physics_process(delta: float) -> void:
 				
 			velocity = dir * SPEED
 			move_and_slide()
+			
 			if (move_points[current_point + 1] - global_position).length() < 4:
 				current_cell = tiles.local_to_map(tiles.to_local(global_position))
 				current_point += 1
+	else:
+		if current_cell == target_cell:
+			paths_node.targeted.erase(target_cell)
+			velocity = Vector2.ZERO
+			if (plants.plant_data.has(target_cell)):
+				if (plants.tree_info.get(plants.plant_data[target_cell]["fruit_name"])):
+					if !tiles.watered_tiles.get(target_cell):
+						if plants.plant_data.get(target_cell):
+							for x in 3:
+								for y in 4:
+									var initial = plants.plant_data[target_cell]["initial"]
+									
+									tiles.watered_tiles[initial + Vector2i(x-1, y-2)] = {"time": 0}
+									watered.set_cell(initial + Vector2i(x-1, y-2), 0, Vector2i.ZERO)
+				else:
+					if !tiles.watered_tiles.get(target_cell):
+						if plants.plant_data.get(target_cell):
+							tiles.watered_tiles[target_cell] = {"time": 0}
+							watered.set_cell(target_cell, 0, Vector2i(0,0))
+				moving = false
+				finished = true
+				current_point = 0
+				target_cell = Vector2i.ZERO
+			
--- nodes/pathMaker.gd
+++ nodes/pathMaker.gd
@@ -11,17 +11,22 @@ var a_star_grid : AStarGrid2D
 	
 func _ready() -> void:
 	a_star_grid = AStarGrid2D.new()
-	a_star_grid.default_compute_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
-	a_star_grid.default_estimate_heuristic = AStarGrid2D.HEURISTIC_MANHATTAN
 	a_star_grid.cell_size = map.tile_set.tile_size 
 	a_star_grid.region = map.get_used_rect() 
 	a_star_grid.update()
+	a_star_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
 
 func _process(delta: float) -> void:
-	for plant in plants.plant_data:
+	
+	for plant in plants.plant_data:	
 		if !map.watered_tiles.get(plant) && targeted.find(plant) == -1:
 			for robot in robots:
 				if robot.finished:
-					targeted.append(plant)
-					robot.initialize(a_star_grid, plant)
+					if plants.tree_info.get(plants.plant_data[plant]["fruit_name"]):
+						if targeted.find(plants.plant_data[plant]["initial"]) == -1: 
+							targeted.append(plants.plant_data[plant]["initial"])
+							robot.initialize(a_star_grid, plants.plant_data[plant]["initial"])
+					else:
+						targeted.append(plant)
+						robot.initialize(a_star_grid, plant)
 					break
