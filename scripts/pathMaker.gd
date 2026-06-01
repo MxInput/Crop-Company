d2478c2d85402e745bfd2712b7803e41dd37c256
MxInput <mxinput@protonmail.com>
2026-06-01 02:02:11 -0400

    Making pest robots function

--- scripts/pathMaker.gd
+++ scripts/pathMaker.gd
@@ -4,8 +4,10 @@ extends Node2D
 @onready var plants: TileMapLayer = get_node("/root/Game/Plants")
 
 @onready var robots = get_tree().get_nodes_in_group("Water_Robots")
+@onready var pestBots = get_tree().get_nodes_in_group("pestBot")
 
 var targeted = []
+var pests_targeted = []
 
 var a_star_grid : AStarGrid2D
 	
@@ -18,14 +20,26 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	for plant in plants.plant_data:	
-		if !map.watered_tiles.get(plant) && targeted.find(plant) == -1:	
+		if !map.watered_tiles.has(plant) && targeted.find(plant) == -1:	
 			for robot in robots:
 				if robot.finished:
 					if plants.tree_info.get(plants.plant_data[plant]["fruit_name"]):
-						if targeted.find(plants.plant_data[plant]["initial"]) == -1: 
-							targeted.append(plants.plant_data[plant]["initial"])
-							robot.initialize(a_star_grid, plants.plant_data[plant]["initial"])
+						if !map.watered_tiles.has(plants.plant_data[plant]["initial"]):
+							if targeted.find(plants.plant_data[plant]["initial"]) == -1: 
+								targeted.append(plants.plant_data[plant]["initial"])
+								robot.initialize(a_star_grid, plants.plant_data[plant]["initial"])
 					else:
 						targeted.append(plant)
+						robot.initialize(a_star_grid, plant)
+					break
+		if map.infected_tiles.has(plant) && !pests_targeted.has(plant):
+			for robot in pestBots:
+				if robot.finished:
+					if plants.tree_info.get(plants.plant_data[plant]["fruit_name"]):
+						if pests_targeted.find(plants.plant_data[plant]["initial"]) == -1: 
+							pests_targeted.append(plants.plant_data[plant]["initial"])
+							robot.initialize(a_star_grid, plants.plant_data[plant]["initial"])
+					else:
+						pests_targeted.append(plant)
 						robot.initialize(a_star_grid, plant)
 					break
